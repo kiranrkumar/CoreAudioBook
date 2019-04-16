d07f3835d4790b6b8a4b4df1897380dcb094d9cf
Kiran Kumar <krkumar11@gmail.com>
2019-04-15 21:48:08 -0700

    Begin stub for file player exercise in Chapter 7

---
+++ CoreAudioBookExamples/III - Advanced Audio/Chapter 7 - Audio Units - Generators Effects and Rendering/Chapter7_FileBasedPlayer.h
@@ -0,0 +1,22 @@
+//
+//  Chapter7_FileBasedPlayer.h
+//  CoreAudioBookExamples
+//
+//  Created by Kiran Kumar on 4/15/19.
+//  Copyright © 2019 Kiran Kumar. All rights reserved.
+//
+
+#ifndef Chapter7_FileBasedPlayer_h
+#define Chapter7_FileBasedPlayer_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    void Chapter7_PlayFromFile(void);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* Chapter7_FileBasedPlayer_h */
---
+++ CoreAudioBookExamples/III - Advanced Audio/Chapter 7 - Audio Units - Generators Effects and Rendering/Chapter7_FileBasedPlayer.mm
@@ -0,0 +1,43 @@
+//
+//  Chapter7_FileBasedPlayer.mm
+//  CoreAudioBookExamples
+//
+//  Created by Kiran Kumar on 4/15/19.
+//  Copyright © 2019 Kiran Kumar. All rights reserved.
+//
+
+#include "Chapter7_FileBasedPlayer.h"
+
+#include <AudioToolbox/AudioToolbox.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma mark - User Data Struct
+    //Listing 7.2
+    
+#pragma mark - Utility functions
+    // Listing 4.2
+    // Listing 7.7 - 7.13
+    // Listing 7.14 - 7.17
+    
+#pragma mark - Entry point
+    void Chapter7_PlayFromFile(void)
+    {
+        // Open the input audio file
+        // Get the audio data format from the file
+        // Listing 7.3
+        
+        // Build a basic fileplayer -> speakers graph
+        // Configure the file player
+        // Listing 7.4
+        
+        // Start playing
+        // Sleep until the file is finished
+        // Listing 7.5 
+    }
+
+#ifdef __cplusplus
+}
+#endif
