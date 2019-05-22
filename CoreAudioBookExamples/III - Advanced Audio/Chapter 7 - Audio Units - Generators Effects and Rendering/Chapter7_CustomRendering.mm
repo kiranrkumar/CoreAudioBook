93e4ac9fa57f7511154233af0de089c4f4dccf7b
Kiran Kumar <krkumar11@gmail.com>
2019-05-21 23:01:49 -0700

    Implemented up through listing 7.34 (finished the render callback - program is now functional)

--- CoreAudioBookExamples/III - Advanced Audio/Chapter 7 - Audio Units - Generators Effects and Rendering/Chapter7_CustomRendering.mm
+++ CoreAudioBookExamples/III - Advanced Audio/Chapter 7 - Audio Units - Generators Effects and Rendering/Chapter7_CustomRendering.mm
@@ -21,6 +21,33 @@ typedef struct MySineWavePlayer {
 
 #pragma mark callback function
 // Listing 7.34
+// follows the signature of AURenderCallback
+OSStatus SineWaveRenderProc(   void *inRefCon,
+                               AudioUnitRenderActionFlags *ioActionFlags,
+                               const AudioTimeStamp *inTimeStamp,
+                               UInt32 inBusNumber,
+                               UInt32 inNumberFrames,
+                               AudioBufferList * __nullable ioData) {
+    MySineWavePlayer *player = (MySineWavePlayer *)inRefCon;
+    
+    double j = player->startingFrameCount;
+    double cycleLength = 44100. / sineFrequency;
+    for (int frame = 0; frame < inNumberFrames; ++frame) {
+        Float32 *data = (Float32 *)ioData->mBuffers[0].mData;
+        (data)[frame] = (Float32)sin(2 * M_PI * (j / cycleLength));
+        // copy to right channel, too
+        data = (Float32 *)ioData->mBuffers[1].mData;
+        (data)[frame] = (Float32)sin(2 * M_PI * (j / cycleLength));
+        
+        j += 1.0;
+        if (j > cycleLength) {
+            j -= cycleLength;
+        }
+    }
+    
+    player->startingFrameCount = j;
+    return noErr;
+}
 
 #pragma mark Utility function
 // Listing 4.2
@@ -46,7 +73,33 @@ static void CheckError(OSStatus error, const char *operation) {
 
 // Listings 7.30 - 7.32
 void CreateAndConnectOutputUnit (MySineWavePlayer *player) {
-
+    // Generate a description that matches the output device (speakers)
+    AudioComponentDescription outputcd = {0};
+    outputcd.componentType = kAudioUnitType_Output;
+    outputcd.componentSubType = kAudioUnitSubType_DefaultOutput;
+    outputcd.componentManufacturer = kAudioUnitManufacturer_Apple;
+    
+    // Get the audio unit
+    AudioComponent comp = AudioComponentFindNext(NULL, &outputcd);
+    if (comp == NULL) {
+        printf("Can't find matching audio unit\n");
+        exit(-1);
+    }
+    CheckError(AudioComponentInstanceNew(comp, &player->outputUnit), "Can't open component for outputUnit");
+    
+    // Now that we have the default output unit, we need to register a custom render callback to the AU
+    AURenderCallbackStruct input;
+    input.inputProc = SineWaveRenderProc; // actual callback function (process)
+    input.inputProcRefCon = player; // context object
+    CheckError(AudioUnitSetProperty(player->outputUnit,
+                                    kAudioUnitProperty_SetRenderCallback,
+                                    kAudioUnitScope_Input,
+                                    0,
+                                    &input,
+                                    sizeof(input)), "Error setting render callback to output AU");
+    
+    // Initialize the unit
+    CheckError(AudioUnitInitialize(player->outputUnit), "Error initialization player's outputUnit");
 }
 
 // Listing 7.29
